f1d5159f4dfae7a1142a13ebbcc48b41f0606f1d
Miles Parker <milesparkercabral@gmail.com>
2026-04-26 16:56:07 -0500

    Initial Commit

---
+++ lil-guy/Managers/game_manager.gd
@@ -0,0 +1,12 @@
+extends Node
+
+#region VARIABLES
+
+
+
+
+
+#endregion
+
+func _change_level(level_name: String):
+	get_tree().change_scene_to_file(LevelList.get(level_name))
---
+++ lil-guy/Managers/level_list.gd
@@ -0,0 +1,4 @@
+extends Node
+
+var test_level: String = "uid://hwdocdycs6e4"
+	
